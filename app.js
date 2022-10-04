10eab6e189aa6eee8840805737fcc30dc1974613
Madina <madinatussupova0820@gmail.com>
2022-10-04 14:21:50 +0600

    post route, update styles

--- app.js
+++ app.js
@@ -78,6 +78,7 @@ app.post("/compose", function (req, res) {
   const post = {
     title: req.body.postTitle,
     content: req.body.postBody,
+    slug: _.kebabCase(req.body.postTitle),
   };
 
   posts.push(post);
